1298bf5e199f78460218a29203050a29d500389d
Reorx <novoreorx@gmail.com>
2023-03-28 23:25:36 +0800

    add interactive elements for options and popup page

--- src/options.ts
+++ src/options.ts
@@ -4,3 +4,8 @@ import { colors, getLogger } from './utils/log';
 const lg = getLogger('options', colors.bgYellowBright)
 
 lg.info('options.ts')
+
+document.querySelector('button')?.addEventListener('click', () => {
+  const numberInput = document.querySelector('input[type=number]') as HTMLInputElement
+  numberInput.value = `${parseInt(numberInput.value) + 1}`
+})
--- src/popup.ts
+++ src/popup.ts
@@ -4,3 +4,8 @@ import { colors, getLogger } from './utils/log';
 const lg = getLogger('popup', colors.bgYellowBright)
 
 lg.info('popup.ts')
+
+document.querySelector('button')?.addEventListener('click', () => {
+  const numberInput = document.querySelector('input[type=number]') as HTMLInputElement
+  numberInput.value = `${parseInt(numberInput.value) + 1}`
+})
